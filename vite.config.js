b7cd555951a86300375864b910957f381bdede64
ihadi5 <85589875+ihadi5@users.noreply.github.com>
2026-03-11 03:05:56 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path"
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  // هذا السطر هو السحر اللي بيحل الشاشة البيضاء
-  base: '/-/', 
-  
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  base: './', 
 })
